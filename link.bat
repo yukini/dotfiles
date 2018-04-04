ad03f83f67a06c388a712cd731d76783ef7c3bef
yuma.mukuna <yuma.mukuna@ilc-v.com>
2018-04-04 09:56:52 +0900

    [add] tern-config

--- link.bat
+++ link.bat
@@ -4,3 +4,4 @@ mklink /H %HOME%\.vimrc .vimrc
 mklink /H %HOME%\.gvimrc .gvimrc
 mklink /H %HOME%\.ideavimrc .ideavimrc
 mklink /H %HOME%\.spacemacs .spacemacs
+mklink /H %HOME%\.tern-config .tern-config
