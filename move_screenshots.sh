34cd6693355b59f7a89a6b459dee2d187fd436de
Yukini <y.mukuna.0724@gmail.com>
2024-02-14 21:40:13 +0900

    small

--- move_screenshots.sh
+++ move_screenshots.sh
@@ -1,4 +1,4 @@
 # 0 10 * * * mv ~/Desktop/スクリーンショット* ~/Desktop/screenshots/
 # 0 10 * * * mv ~/Desktop/Screen\ Shot* ~/Desktop/screenshots/
-mv ~/Desktop/スクリーンショット* ~/Desktop/screenshots/
-mv ~/Desktop/Screen\ Shot* ~/Desktop/screenshots/
+mv ~/Desktop/スクリーンショット* ~/Documents/screenshots/
+mv ~/Desktop/Screen\ Shot* ~/Documents/screenshots/
