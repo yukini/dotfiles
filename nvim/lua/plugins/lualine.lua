59a54b80b2e81b267ae9839f8b636c6b0584cafc
Yukini <y.mukuna.0724@gmail.com>
2024-05-25 16:40:46 +0900

    20240525

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -8,9 +8,33 @@ return {
       require('lualine').setup({
         sections = {
           -- left
-          lualine_a = { { 'mode', icons_enabled = false, padding = { left = 0, right = 0 }, fmt = function() return " " end } },
-          lualine_b = { { 'branch', icons_enabled = false, padding = { left = 1, right = 0 } }, { 'diff' }, 'diagnostics' },
-          lualine_c = { 'filename' },
+          lualine_a = {
+            {
+              'mode',
+              icons_enabled = false,
+              padding = { left = 0, right = 0 },
+              fmt = function() return " " end
+            }
+          },
+          lualine_b = {
+            {
+              'branch',
+              icons_enabled = false,
+              padding = {
+                left = 1, right = 0 }
+            },
+            {
+              'diff'
+            },
+            'diagnostics'
+          },
+          lualine_c = {
+            {
+              'filename',
+              file_status = true, -- displays file status (readonly status, modified status)
+              path = 2            -- 0 = just filename, 1 = relative path, 2 = absolute path
+            },
+          },
 
           -- right
           lualine_x = { { 'encoding' }, { 'fileformat', icons_enabled = false }, { 'filetype', icons_enabled = false } },
