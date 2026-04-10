a48550610b3be286205a271a5d93a07d6524848b
Yukini <y.mukuna.0724@gmail.com>
2026-04-10 23:26:55 +0900

    chore(nvim): migrate from nvim-cmp to blink.cmp

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,62 +1,17 @@
 return {
   {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-nvim-lsp-signature-help",
-      "hrsh7th/cmp-nvim-lsp-document-symbol",
-
-      "hrsh7th/cmp-path",
-      "hrsh7th/cmp-buffer",
-      "hrsh7th/cmp-cmdline",
-      "hrsh7th/cmp-emoji",
+    'saghen/blink.cmp',
+    dependencies = 'rafamadriz/friendly-snippets',
+    version = '*',
+    opts = {
+      keymap = { preset = 'default' },
+      appearance = {
+        use_nvim_cmp_as_default = true,
+        nerd_font_variant = 'mono'
+      },
+      signature = { enabled = true },
     },
-    config = function()
-      local status, cmp = pcall(require, "cmp")
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            vim.snippet.expand(args.body)
-          end,
-        },
-        sources = cmp.config.sources({
-            { name = "nvim_lsp" },
-
-            { name = "nvim_lsp_signature_help" },
-            { name = 'nvim_lsp_document_symbol' },
-          },
-          {
-            { name = "path" },
-            { name = "buffer" },
-          }),
-        mapping = cmp.mapping.preset.insert({
-          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-          ['<C-f>'] = cmp.mapping.scroll_docs(4),
-          ['<C-Space>'] = cmp.mapping.complete(),
-          ['<C-e>'] = cmp.mapping.close(),
-          ['<CR>'] = cmp.mapping.confirm({
-            behavior = cmp.ConfirmBehavior.Replace,
-            select = true
-          }),
-        }),
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(':', {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = cmp.config.sources({
-            { name = 'path' }
-          }, {
-            { name = 'cmdline' }
-          })
-        }),
-        cmp.setup.cmdline('/', {
-          mapping = cmp.mapping.preset.cmdline(),
-          sources = {
-            { name = 'buffer' }
-          }
-        }),
-
-      })
-    end
+    opts_extend = { "sources.default" },
   },
   {
     "neovim/nvim-lspconfig",
@@ -66,10 +21,26 @@ return {
       "nvim-lua/plenary.nvim",
     },
     config = function()
-      require 'mason'.setup()
-      require 'mason-lspconfig'.setup {
+      require('mason').setup()
+      require('mason-lspconfig').setup({
         ensure_installed = { "lua_ls", "marksman" },
-      }
+      })
+
+      require('mason-lspconfig').setup_handlers({
+        function(server_name)
+          local opts = {
+            capabilities = require('blink.cmp').get_lsp_capabilities(),
+          }
+          if server_name == "lua_ls" then
+            opts.settings = {
+              Lua = {
+                diagnostics = { globals = { 'vim' } }
+              }
+            }
+          end
+          require('lspconfig')[server_name].setup(opts)
+        end
+      })
 
       -- Global mappings.
       -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -93,12 +64,6 @@ return {
           vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
           vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-          -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
-          -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
-          -- vim.keymap.set('n', '<space>wl', function()
-          --   print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-          -- end, opts)
-          -- vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
           vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
           vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
           vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
@@ -108,27 +73,5 @@ return {
         end,
       })
     end,
-    -- init = function()
-    --   local capabilities = vim.lsp.protocol.make_client_capabilities()
-    --   local on_attach = function(_, bufnr)
-    --     vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
-    --       vim.lsp.buf.format()
-    --     end, { desc = 'Format current buffer with LSP' })
-    --   end
-    --   -- require 'mason-lspconfig'.setup_handlers {
-    --   --   function(server_name)
-    --   --     require('lspconfig')[server_name].setup {
-    --   --       capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities),
-    --   --       on_attach = on_attach,
-    --   --       settings = {
-    --   --         Lua = {
-    --   --           -- https://neovim.discourse.group/t/how-to-suppress-warning-undefined-global-vim/1882/6
-    --   --           diagnostics = { globals = { 'vim' } }
-    --   --         }
-    --   --       }
-    --   --     }
-    --   --   end
-    --   -- }
-    -- end
   },
 }
